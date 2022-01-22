04754300dd2326a13bd0355ccadb27deebb3e4a8
dipesh shakya <dipeshshakya106@gmail.com>
2022-01-22 18:55:41 +1100

    single product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        {/* <Home /> */}
+        {/* <ProductList /> */}
+        <Product />
       </header>
     </div>
   );
